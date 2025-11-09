ba2c99ad9805e41b6d6505eac9bdb5cdaecf7850
crittheoryworkgroup <154104315+crittheoryworkgroup@users.noreply.github.com>
2025-11-09 00:13:43 +0000

    Deploying to gh-pages from @ CTWGwebsite/CTWGwebsite.github.io@5c79e282b52f6bbcca9e4b4c6f0b9809095f3407 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,7 +37,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/people/";
           },
-        },{id: "post-reflections-on-the-right-and-its-violence-part-i",
+        },{id: "post-reflections-on-the-right-and-its-violence-part-ii",
+      
+        title: "Reflections on the Right and its Violence, Part II",
+      
+      description: "A Theoretical Investigation into Reactionary Violence",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2025/ReactionPt2/";
+        
+      },
+    },{id: "post-reflections-on-the-right-and-its-violence-part-i",
       
         title: "Reflections on the Right and its Violence, Part I",
       
